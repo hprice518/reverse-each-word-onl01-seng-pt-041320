c83bb892744e04c03b488ab83dec563fa4313582
Harrison Price <hprice518@gmail.com>
2020-04-25 18:38:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(hello)
   hello_array = hello.split (" ")
 
 reverse_array = []
-  phrase_split.collect do |word|
-  reverse_array << "word.reverse"
+  hello_array.collect do |word|
+  reverse_array << "hello_array.reverse"
   end
   reverse_array.join
 end
